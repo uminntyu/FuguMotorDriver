5a62a55139ade06ba08a42b62c21122e4aca079a
hhyuga <hhyuga201515@gmail.com>
2016-09-28 15:50:08 +0900

    ライブラリとして使用できるように変更

---
+++ FuguMotorDriver.h
@@ -0,0 +1,31 @@
+
+#include "Arduino.h"
+
+class FuguMotorDriver
+{
+private:
+    enum STATESBYTE {
+        DRIVEROFF = 0x80,
+        DRIVERON = 0x90
+    };
+
+    enum DATABYTE {
+        FUGU_SHUTDOWN = 0x00,
+        FUGU_CW = 0x01,
+        FUGU_CCW = 0x02
+    };
+
+    int ch;
+
+    void cw( int speed );
+    void ccw( int speed );
+    //void stop();
+
+    HardwareSerial *serial;
+
+public:
+    FuguMotorDriver( HardwareSerial *serial, int ch );
+    void move( int speed );
+    void stop();
+
+};
